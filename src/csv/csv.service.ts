6741779fe3bc4cc3ff5230ae250f65b9bb20f33a
ionblak <ihor.ionashku@gmail.com>
2023-12-07 13:58:54 +0200

    test

--- src/csv/csv.service.ts
+++ src/csv/csv.service.ts
@@ -592,6 +592,7 @@ export class CsvService {
             const update = { $set: { carrier: carrier, type: type } };
 
             bulkOps.push(filter, update, { upsert: true });
+            console.log(bulkOps);
           }
           await this.apiResultModel.bulkWrite(bulkOps);
           await this.baseModel.bulkWrite(bulkOps);
